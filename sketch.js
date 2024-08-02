f419cf3c93c76ad7e3aad30b0a0db2c8d57217e8
rihannamedeiros <00001124337891sp@al.educacao.sp.gov.br>
2024-08-02 18:16:03 -0300

    adicionando arquivos js e css

---
+++ sketch.js
@@ -0,0 +1,15 @@
+function setup() {
+    createCanvas(400, 400);
+  }
+  
+  function draw() {
+    background(220);
+  }
+   function draw(){
+     stroke("blue ")
+     fill("red");
+     
+     if (mouseIsPressed){
+       rect (mouseX, mouseY, 20,35);
+     }
+   }
